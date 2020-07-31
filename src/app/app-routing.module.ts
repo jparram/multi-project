de29448d86c422c39aa0d9851c2872bb5f89353c
John Parramore <john.parramore@amwins.com>
2020-07-31 16:19:54 -0400

    updates with all of the initial shenanigans

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { PropertyStartComponent } from './../../projects/property/src/app/property-start/property-start.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GlStartComponent } from 'projects/general-liability/src/app/gl-start/gl-start.component';
+import { PackageStartComponent } from 'projects/package/src/app/package-start/package-start.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'access/gl', component: GlStartComponent },
+ { path: 'access/property', component: PropertyStartComponent },
+ { path: 'access/package', component: PackageStartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { PropertySharedModule } from './../../projects/property/src/app/app.module';
+import { PackageSharedModule } from './../../projects/package/src/app/app.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralLiabilitySharedModule } from 'projects/general-liability/src/app/app.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GeneralLiabilitySharedModule,
+    PackageSharedModule,
+    PropertySharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
